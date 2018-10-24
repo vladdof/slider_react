7aac14f99f4e4c5fdbf09b073906eaff08b75814
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-10-24 23:38:40 +0400

    Создание шапки и меню

--- src/App.js
+++ src/App.js
@@ -3,17 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL+'/img/bg.png'
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Ivan" surname="Family" link="vk.com" />
-        <SayFullName name="Vlad" surname="Asd" link="facebook.com" />
-        <SayFullName name="Lena" surname="Cok" link="#" />
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
       </div>
     );
   }
